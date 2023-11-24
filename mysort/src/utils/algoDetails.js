0ab7cd2bfde0aa60c069fd86d28245d45c210770
Priyadharshini S <priyadharshinisathasivam01@gmail.com>
2023-11-24 23:34:46 +0530

    algo details is updated

--- mysort/src/utils/algoDetails.js
+++ mysort/src/utils/algoDetails.js
@@ -12,7 +12,7 @@ export const algoDetails=
         {
             id:"2",
             name:"Quick sort",
-            description:"Quick sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.",
+            description:"QuickSort is a sorting algorithm based on the Divide and Conquer algorithm that picks an element as a pivot and partitions the given array around the picked pivot by placing the pivot in its correct position in the sorted array.",
             BCTC:"O(nlogn)",
             ACTC:"O(nlogn)",
             WCTC:"O(n^2)",
@@ -21,38 +21,38 @@ export const algoDetails=
         {
             id:"3",
             name:"Selection sort",
-            description:"Selection Sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.",
-            BCTC:"O(nlogn)",
-            ACTC:"O(nlogn)",
+            description:"Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list. ",
+            BCTC:"O(n^2)",
+            ACTC:"O(n^2)",
             WCTC:"O(n^2)",
-            WCSC:"O(nlogn)"
+            WCSC:"O(1)"
         },
         {
             id:"4",
             name:"Insertion sort",
-            description:"Insertion Sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.",
-            BCTC:"O(nlogn)",
-            ACTC:"O(nlogn)",
+            description:"Insertion Sort works by comparing an element with the elements to its left, until it reaches an element that is smaller than it; the element is then inserted in front of the smaller element.",
+            BCTC:"O(n)",
+            ACTC:"O(n^2)",
             WCTC:"O(n^2)",
-            WCSC:"O(nlogn)"
+            WCSC:"O(1)"
         },
         {
             id:"5",
             name:"Bubble sort",
-            description:"Bubble Sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.",
-            BCTC:"O(nlogn)",
-            ACTC:"O(nlogn)",
+            description:"Bubble sort algorithm is an algorithm that sorts an array by comparing two adjacent elements and swapping them if they are not in the intended order. Here order can be anything like increasing or decreasing.",
+            BCTC:"O(n)",
+            ACTC:"O(n^2)",
             WCTC:"O(n^2)",
-            WCSC:"O(nlogn)"
+            WCSC:"O(1)"
         },
         {
             id:"6",
             name:"Heap sort",
-            description:"Heap Sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.",
+            description:"Heap sort is a comparison-based sorting technique based on Binary Heap data structure. It is similar to the selection sort where we first find the minimum element and place the minimum element at the beginning. Repeat the same process for the remaining elements.",
             BCTC:"O(nlogn)",
             ACTC:"O(nlogn)",
-            WCTC:"O(n^2)",
-            WCSC:"O(nlogn)"
+            WCTC:"O(nlogn)",
+            WCSC:"O(1)"
         }
 
     ]
